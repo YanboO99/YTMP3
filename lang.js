872054e47efda5caa4e0e217b8f062d3eb05a8e8
YanboO99 <108884523+YanboO99@users.noreply.github.com>
2022-07-09 11:19:59 +0700

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,5 @@
+var checking = 'memeriksa video',
+    loading = 'memuat video',
+    converting = 'mengkonversi video',
+    dload = 'Unduh',
+    error_msg = '<div id="error"><p>Terjadi kesalahan (kode: code1-code2).</p><p>Silakan coba mengonversi video lain dengan mengklik <a href="">di sini</a> atau coba unduh <a href="https://js_url/e" rel="nofollow" target="_blank">di sini</a>.</p></div>';
